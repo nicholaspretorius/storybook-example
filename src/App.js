b06a449d0a42e86d11031e03b867002447d8b76c
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-27 10:49:55 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Task from "./components/Task";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>Hello world!</h3>
+        <Task />
         <a
           className="App-link"
           href="https://reactjs.org"
